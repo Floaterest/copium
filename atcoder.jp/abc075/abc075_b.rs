995058a84b9d3e23b04d4d3697f9c5296eb33dad
floaterest <56704092+floaterest@users.noreply.github.com>
2022-02-26 21:32:57 -0400

    update(atcoder) abc075 b
    https://atcoder.jp/contests/abc075/tasks/abc075_b

--- atcoder.jp/abc075/abc075_b.rs
+++ atcoder.jp/abc075/abc075_b.rs
@@ -41,17 +41,19 @@ mod reader {
             self.prepare();
             self.tokens.next().unwrap().parse().ok().unwrap()
         }
+
+        pub fn i(&mut self) -> i32 { self.next::<i32>() }
+        pub fn f(&mut self) -> f32 { self.next::<f32>() }
+        pub fn us(&mut self) -> usize { self.next::<usize>() }
+        pub fn str(&mut self) -> String { self.next::<String>() }
+        pub fn bytes(&mut self) -> Vec<u8> { self.next::<String>().into_bytes() }
     }
 
     macro_rules! re {
         // re!(r, [i32;20]) -> Vec<i32> (with len 20)
         ($r:expr, [$type:ty; $len:expr]) => ((0..$len).map(|_| re!($r, $type)).collect::<Vec<$type>>());
-        // re!(r, usize) -> usize
-        ($r:expr, $type:ty) => ($r.next::<$type>());
         // re!(r, usize, i32, String) -> (usize, i32, String)
-        ($r:expr, $($type:ty),*) => (($(re!($r, $type)),*));
-        // let s:String = re!(r)
-        ($r:expr) => ($r.next());
+        ($r:expr, $($type:ty),+) => (($($r.next::<$type>()),+));
     }
 }
 
@@ -62,33 +64,38 @@ mod writer {
 
     //#region Writable
     pub trait Writable<Mode> {
-        fn write_to<W: Write>(self, w: &mut W);
+        fn write_to<W: Write>(self, w: &mut W, sep: &str, end: &str);
     }
 
     #[non_exhaustive]
-    pub struct One;
+    pub struct Slice;
 
     #[non_exhaustive]
     pub struct Many;
 
     #[non_exhaustive]
-    pub struct Slice;
+    pub struct One;
 
-    impl<T: Display> Writable<One> for T {
-        fn write_to<W: Write>(self, w: &mut W) {
-            write!(w, "{}", self).unwrap()
+    impl<T: Display> Writable<Slice> for &[T] {
+        fn write_to<W: Write>(self, w: &mut W, sep: &str, end: &str) {
+            self.iter().write_to(w, sep, end);
         }
     }
 
     impl<I> Writable<Many> for I where I: Iterator, I::Item: Display {
-        fn write_to<W: Write>(mut self, w: &mut W) {
-            self.for_each(|val| write!(w, "{}", val).unwrap());
+        fn write_to<W: Write>(mut self, w: &mut W, sep: &str, end: &str) {
+            if let Some(val) = self.next() {
+                write!(w, "{}", val).unwrap();
+            } else { return; }
+
+            self.for_each(|val| write!(w, "{}{}", sep, val).unwrap());
+            write!(w, "{}", end).unwrap();
         }
     }
 
-    impl<T: Display> Writable<Slice> for &[T] {
-        fn write_to<W: Write>(self, w: &mut W) {
-            self.iter().write_to(w);
+    impl<T: Display> Writable<One> for T {
+        fn write_to<W: Write>(self, w: &mut W, sep: &str, end: &str) {
+            write!(w, "{}{}", self, end).unwrap()
         }
     }
     //#endregion Writable
@@ -100,20 +107,37 @@ mod writer {
     }
 
     impl<W: Write> Writer<W> {
-        pub fn new(w: W) -> Self {
-            Self { writer: BufWriter::new(w) }
+        pub fn new(w: W) -> Self { Self { writer: BufWriter::new(w) } }
+
+        pub fn yneos(&mut self, b: bool) {
+            self.writer.write((if b { "yes" } else { "no" }).as_bytes()).unwrap();
         }
-        pub fn write<M, T: Writable<M>>(&mut self, val: T) {
-            val.write_to(&mut self.writer);
-            self.writer.write("\n".as_bytes()).unwrap();
+
+        pub fn w<M, T: Writable<M>>(&mut self, val: T) {
+            val.write_to(&mut self.writer, "", "");
+        }
+        pub fn wn<M, T: Writable<M>>(&mut self, val: T) {
+            val.write_to(&mut self.writer, "", "\n");
+        }
+        pub fn ws<M, T: Writable<M>>(&mut self, val: T) {
+            val.write_to(&mut self.writer, " ", "");
+        }
+        pub fn wsn<M, T: Writable<M>>(&mut self, val: T) {
+            // `ws(&[a,b,c][..])` to write multiple variables
+            val.write_to(&mut self.writer, " ", "\n");
+        }
+        pub fn write<M, T: Writable<M>>(&mut self, val: T, sep: &str, end: &str) {
+            val.write_to(&mut self.writer, sep, end);
         }
     }
     //#endregion Writer
-    macro_rules! wr {
-        ($w:expr, $val:expr) => ($w.write($val));
-    }
-    macro_rules! yneos {
-        ($w:expr, $bool:expr) => ($w.write(if $bool { "yes" } else { "no" }));
+
+    macro_rules! wsn {
+        ($w:expr, $first:expr, $($val:expr),*) => {
+            $w.w($first);
+            ($(write!($w.writer, " {}", $val).unwrap()),*);
+            $w.writer.write("\n".as_bytes()).unwrap();
+        };
     }
 }
 //#endregion mod
@@ -126,23 +150,19 @@ use writer::Writer;
 const d8: [(i32, i32); 8] = [(-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1)];
 //#endregion constant
 
-fn solve<R: Read, W: Write>(mut r: Reader<R>, mut w: Writer<W>) {
-    let (H, W) = re!(r,usize,usize);
-    let mut map = vec![0; H + 2];
-    // create H+2 and W+2 field
-    (1..=H).for_each(
-        |y| re!(r,String).bytes().for_each(
-            |b| map[y] = (map[y] + (b == b'#') as usize) << 1
-        )
+fn solve<R: Read, W: Write>(mut re: Reader<R>, mut wr: Writer<W>) {
+    let mut map = [[b'_'; 52]; 52];
+    let (h, w) = re!(re,usize,usize);
+    (1..=h).for_each(
+        |y| re.bytes().iter().enumerate().for_each(|(x, b)| map[y][x + 1] = *b)
     );
-
-    (1..=H).for_each(
-        |y| wr!(w, (1..=W).rev().map(
-            |x| if (map[y] >> x & 1) == 1 { '#' } else {
+    map[..=h].iter().enumerate().skip(1).for_each(
+        |(y, r)| wr.wn((*r)[..=w].iter().enumerate().skip(1).map(
+            |(x, b)| if b == &b'#' { '#' } else {
                 (d8.iter().map(
                     |(dx, dy)| ((x as i32 + dx) as usize, (y as i32 + dy) as usize)
                 ).filter(
-                    |(x, y)| map[*y] >> x & 1 == 1
+                    |(x, y)| map[*y][*x] == b'#'
                 ).count() as u8 + b'0') as char
             }
         ))
