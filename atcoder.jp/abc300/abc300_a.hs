ef2c0f7d0af0fb8911da2f87047f9a87b69e2933
floaterest <floaterest@users.noreply.github.com>
2023-06-02 19:33:41 -0400

    update(abc300) abc300_a
    https://atcoder.jp/contests/abc300/tasks/abc300_a

--- atcoder.jp/abc300/abc300_a.hs
+++ atcoder.jp/abc300/abc300_a.hs
@@ -1,15 +1,8 @@
 -- https://atcoder.jp/contests/abc300/tasks/abc300_a
-{-# OPTIONS_GHC -Wno-compat-unqualified-imports #-}
-{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
-{-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
-
 import Data.List
 
 main :: IO ()
-main = interact $ solve . lines
+main= interact $ show . solve . (<$>) read . words
 
-solve :: [String] -> String
-solve s = show $ maybe 0 (+1) $ elemIndex sum arr
-  where
-    (_ : a : b : arr) = map read (s >>= words) :: [Int]
-    sum = a + b
+solve :: [Integer] -> Int
+solve (_ : a : b : cs) = maybe 0 (+ 1) $ elemIndex (a + b) cs
