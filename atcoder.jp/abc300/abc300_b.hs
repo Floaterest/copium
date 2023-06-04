b7f8fdd040ce31b81fac953243d026a47680b408
floaterest <floaterest@users.noreply.github.com>
2023-06-04 19:58:10 -0400

    add(abc300) abc300_b
    https://atcoder.jp/contests/abc300/tasks/abc300_b

---
+++ atcoder.jp/abc300/abc300_b.hs
@@ -0,0 +1,23 @@
+-- https://atcoder.jp/contests/abc300/tasks/abc300_b
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Use infix" #-}
+import Data.List
+import Data.Maybe
+
+yes :: Bool -> String
+yes True = "Yes"
+yes False = "No"
+
+main :: IO ()
+main = interact $ yes . solve . lines
+
+solve :: [String] -> Bool
+solve (hw : ab) = elem b as
+  where
+    as = [d | c <- rotate h a, d <- transpose $ map (rotate w) c]
+    [h, w] = read <$> words hw
+    (a, b) = splitAt h ab
+
+rotate :: Int -> [a] -> [[a]]
+rotate n a = take n $ (map . take) n $ tails $ cycle a
