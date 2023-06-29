757db15d90d3bf3becbdf8715c63d488d5b5082a
floaterest <floaterest@users.noreply.github.com>
2023-06-29 18:48:19 -0400

    update(abc302) abc302_b.hs
    https://atcoder.jp/contests/abc302/tasks/abc302_b

--- atcoder.jp/abc302/abc302_b.hs
+++ atcoder.jp/abc302/abc302_b.hs
@@ -14,6 +14,8 @@ import Control.Applicative
 import Control.Arrow
 import Control.Monad
 import Data.Array
+import Data.Foldable
+import Data.Maybe
 import Data.Tuple (swap)
 import Debug.Trace (trace)
 
@@ -22,17 +24,15 @@ type C = Char
 type I = Integer
 type S = String
 
-yes :: B -> S
-yes True = "Yes\n"
-yes False = "No\n"
+-- yes :: B -> S
+-- yes True = "Yes\n"
+-- yes False = "No\n"
 
 main :: IO ()
 main = interact $ unlines . fmap show2 . please . words
   where
     please (h : w : xs) = solve (read h) (read w) xs
     show2 (x, y) = unwords $ show <$> [x, y]
-
--- show2 (x, y) = show (y + 1) ++ " " ++ show (x + 1)
 
 d8 :: [(Int, Int) -> (Int, Int)]
 d8 = add <$> ds
@@ -42,17 +42,15 @@ d8 = add <$> ds
 
 type A = Array (Int, Int) Char
 solve :: Int -> Int -> [[Char]] -> [(Int, Int)]
-solve h w cs = head aa5
+solve h w cs = fromMaybe [] a5
   where
-    -- is = index2d w h cs
-    bs = toArray h w cs
-    as = indices bs
-    a1 = filter fs as
-    a5 = filter fi [take 5 $ iterate d p1 | p1 <- a1, d <- d8]
-    aa5 = filter fn a5
-    fs = (== 's') . (bs !)
-    fi = inRange (bounds bs) . last
-    fn = (== "snuke") . fmap (bs !)
+    as = toArray h w cs
+    a1 = filter ((== 's') . (as !)) (indices as)
+    t5 d = take 5 . iterate d
+    a5 = find (und . (fin &&& fsn)) (t5 <$> d8 <*> a1)
+    fin = inRange (bounds as) . last
+    fsn = (== "snuke") . fmap (as !)
+    und (b1, b2) = b1 && b2 -- Deutch goes brr
 
 -- | 2d list to array indexd by (x, y)
 toArray :: Int -> Int -> [[a]] -> Array (Int, Int) a
