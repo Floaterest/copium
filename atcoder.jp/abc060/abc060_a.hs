63bc00248461ac504a7c8b25cbab5ab6ccf3152a
floaterest <floaterest@users.noreply.github.com>
2023-07-01 20:24:10 -0400

    add(abc060) abc060_a.hs
    https://atcoder.jp/contests/abc060/tasks/abc060_a

---
+++ atcoder.jp/abc060/abc060_a.hs
@@ -0,0 +1,39 @@
+-- https://atcoder.jp/contests/abc060/tasks/abc060_a
+-- https://atcoder.jp/contests/abc303/tasks/abc303_a
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+{-# HLINT ignore "Use infix" #-}
+{-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
+{-# OPTIONS_GHC -Wno-type-defaults #-}
+{-# OPTIONS_GHC -Wno-unqualified-imports #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# OPTIONS_GHC -Wno-unrecognised-warning-flags #-}
+{-# OPTIONS_GHC -Wno-unused-imports #-}
+{-# OPTIONS_GHC -Wno-unused-top-binds #-}
+
+import Control.Applicative
+import Control.Arrow
+import Control.Monad
+import Data.Array
+import Data.Char
+import Data.Foldable
+import Data.List
+import Data.Maybe
+import Data.Tuple (swap)
+import Debug.Trace (trace)
+
+type B = Bool
+type C = Char
+type I = Integer
+type S = String
+
+yes :: B -> S
+yes True = "Yes\n"
+yes False = "No\n"
+
+main :: IO ()
+main = interact $ fmap toUpper . yes . solve . words
+  where
+    solve = compute
+
+compute :: [S] -> B
+compute [a, b, c] = last a == head b && last b == head c
