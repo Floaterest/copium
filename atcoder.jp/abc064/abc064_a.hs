c2484b2e1273af8345119f384a77417869e3b1f3
floaterest <floaterest@users.noreply.github.com>
2023-07-05 19:52:48 -0400

    add(abc064) abc064_a.hs
    https://atcoder.jp/contests/abc064/tasks/abc064_a

---
+++ atcoder.jp/abc064/abc064_a.hs
@@ -0,0 +1,40 @@
+-- https://atcoder.jp/contests/abc064/tasks/abc064_a
+{-# OPTIONS_GHC -Wno-incomplete-patterns -Wno-unrecognised-pragmas -Wno-unused-imports -Wno-unused-top-binds #-}
+
+{-# HLINT ignore "Use infix" #-}
+
+import Control.Applicative
+import Control.Arrow
+import Control.Monad
+import Data.Array
+import Data.Char
+import Data.Foldable
+import Data.List
+import Data.Maybe
+import Data.Tuple (swap)
+import Debug.Trace (trace)
+
+type B = Bool
+type C = Char
+type I = Integer
+type S = String
+
+-- | answer Yes or No
+yes :: B -> S
+yes True = "Yes\n"
+yes False = "No\n"
+
+-- | read @[Integer]@
+ints :: S -> [I]
+ints = fmap read . words
+
+-- | pairwise zipWith
+pairWith :: (a -> a -> b) -> [a] -> [b]
+pairWith = (<*> tail) . zipWith
+
+main :: IO ()
+main = interact $ fmap toUpper . yes . solve . ints
+  where
+    solve = aa
+
+aa [a,b,c] = rem (b * 10 + c) 4 == 0
