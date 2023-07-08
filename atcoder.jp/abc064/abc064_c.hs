b15333bebd811735660475149caa62de8f19812a
floaterest <floaterest@users.noreply.github.com>
2023-07-08 18:14:14 -0400

    add(abc064) abc064_c.hs
    https://atcoder.jp/contests/abc064/tasks/abc064_c

---
+++ atcoder.jp/abc064/abc064_c.hs
@@ -0,0 +1,56 @@
+-- https://atcoder.jp/contests/abc064/tasks/abc064_c
+{-# OPTIONS_GHC -Wno-incomplete-patterns -Wno-unrecognised-pragmas -Wno-unused-imports -Wno-unused-top-binds #-}
+
+{-# HLINT ignore "Use infix" #-}
+
+import Control.Applicative
+import Control.Arrow
+import Control.Monad
+import Data.Array
+import Data.Char
+import Data.Foldable
+import Data.List
+import Data.Maybe
+import Data.Tuple (swap)
+import Debug.Trace (trace)
+
+type B = Bool
+type C = Char
+type I = Integer
+type S = String
+
+-- | answer Yes or No
+yes :: B -> S
+yes True = "Yes\n"
+yes False = "No\n"
+
+-- | read @[Integer]@
+ints :: S -> [I]
+ints = fmap read . words
+{-# INLINE ints #-}
+
+-- | pairwise zipWith
+pairWith :: (a -> a -> b) -> [a] -> [b]
+pairWith = (<*> tail) . zipWith
+{-# INLINE pairWith #-}
+
+divides :: Integral a => a -> a -> B
+divides = ((0 ==) .) . flip rem
+{-# INLINE divides #-}
+
+count :: (a -> Bool) -> [a] -> Int
+count f = length . filter f
+
+main :: IO ()
+main = interact $ tostr . solve . parse
+  where
+    parse = ints
+    solve = cc . tail
+    tostr = unwords . fmap show
+
+cc :: [I] -> [Int]
+cc ns = [max 1 a, a + b]
+  where
+    as = (`div` 400) <$> ns
+    b = count (>= 8) as
+    a = (length . group . sort . filter (< 8)) as
