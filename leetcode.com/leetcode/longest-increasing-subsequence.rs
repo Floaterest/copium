db4e713088d201c04000a47c1d9592b0dc4191cb
floaterest <floaterest@users.noreply.github.com>
2023-11-17 19:31:35 -0500

    update(leetcode) longest-increasing-subsequence.rs (binary search)
    https://leetcode.com/problems/longest-increasing-subsequence/

--- leetcode.com/leetcode/longest-increasing-subsequence.rs
+++ leetcode.com/leetcode/longest-increasing-subsequence.rs
@@ -1,22 +1,25 @@
 // https://leetcode.com/problems/longest-increasing-subsequence/
 struct Solution;
+
 impl Solution {
     pub fn length_of_lis(ns: Vec<i32>) -> i32 {
-        let mut dp = vec![1; ns.len()];
-        for k in 0..ns.len() {
-            dp[k] = 1 + dp
-                .iter()
-                .take(k)
-                .enumerate()
-                .filter(|&(i, _)| ns[i] < ns[k])
-                .max_by_key(|&(_, n)| n)
-                .unwrap_or((0, &0))
-                .1;
-            dbg!(&dp);
+        let mut arr = Vec::with_capacity(ns.len());
+        for n in &ns {
+            match arr.binary_search(n) {
+                Ok(_) => {}
+                Err(i) => {
+                    if i == arr.len() {
+                        arr.push(*n);
+                    } else {
+                        arr[i] = *n;
+                    }
+                }
+            }
         }
-        *dp.iter().max().unwrap_or(&0)
+        arr.len() as i32
     }
 }
+
 fn main() {
     assert_eq!(Solution::length_of_lis(vec![10, 9, 2, 5, 3, 7, 101, 18]), 4);
     assert_eq!(Solution::length_of_lis(vec![0, 1, 0, 3, 2, 3]), 4);
