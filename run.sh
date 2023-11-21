972ef6334c09aa1aa8fa26fd9ebfdee14371a164
floaterest <floaterest@users.noreply.github.com>
2023-11-21 18:05:12 -0500

    refactor(run) detect project once

--- run.sh
+++ run.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
-run() {
-    bat input.txt
-	if [ -f "package.yaml" ]; then
-		stack run
-	elif [ -f "Cargo.toml" ]; then
-		cargo run
-	else
-		echo 'No project found' 1>&2;
-	fi < input.txt | bat --file-name output.txt
-}
+# set copy and paste command
+case "$XDG_SESSION_TYPE" in
+    x11)
+        xc() { xclip -selection clipboard; }
+        xo() { xclip -selection clipboard -o; };;
+    wayland)
+        xc() { wl-copy; }
+        xo() { wl-paste -t 'text/plain;charset=utf-8;'; };;
+    *) echo "Error: Unsupported session type '$XDG_SESSION_TYPE'." ;;
+esac
+
+if [ -f "package.yaml" ]; then
+    run() { stack run; }
+    code='src/Main.hs'
+elif [ -f "Cargo.toml" ]; then
+    run() { cargo run; }
+    code='src/main.rs'
+else
+    echo 'No project found' 1>&2;
+fi
 
 input=$1
 while [[ "$input" != 'q' ]]; do
@@ -17,7 +27,8 @@ while [[ "$input" != 'q' ]]; do
         q)
             exit 0 ;;
         r)
-            run ;;
+            bat input.txt
+            run < input.txt | bat --file-name output.txt ;;
     esac
     printf '>>> '
     read input
