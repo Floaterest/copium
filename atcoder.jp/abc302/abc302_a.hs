4714708b804c16c9e2aeb2c1793913b28f5bdf27
floaterest <floaterest@users.noreply.github.com>
2023-11-25 16:06:34 -0500

    update(abc302) abc302_a.hs (Readable typeclass)
    https://atcoder.jp/contests/abc302/tasks/abc302_a

--- atcoder.jp/abc302/abc302_a.hs
+++ atcoder.jp/abc302/abc302_a.hs
@@ -1,33 +1,57 @@
 -- https://atcoder.jp/contests/abc302/tasks/abc302_a
-{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE TupleSections #-}
 {-# HLINT ignore "Use infix" #-}
+{-# OPTIONS_GHC -Wno-incomplete-patterns -Wno-unrecognised-pragmas -Wno-unused-imports -Wno-unused-top-binds #-}
 {-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
-{-# OPTIONS_GHC -Wno-type-defaults #-}
-{-# OPTIONS_GHC -Wno-unqualified-imports #-}
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-{-# OPTIONS_GHC -Wno-unused-imports #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
 
-import Control.Arrow hiding (arr)
+import Control.Applicative
+import Control.Arrow
 import Control.Monad
+import Data.Array
+import qualified Data.ByteString.Char8 as BS
+import Data.Char
+import Data.Foldable
 import Data.List
-import Data.Tuple (swap)
-import Debug.Trace (trace)
-import GHC.Arr
+import Data.Maybe
+import Prelude hiding (read)
 
-type B = Bool
-type C = Char
-type I = Integer
-type S = String
+type ByteString = BS.ByteString
 
-yes :: B -> S
-yes True = "Yes\n"
-yes False = "No\n"
+--- Parser
+newtype Parser a = Parser ([ByteString] -> (a, [ByteString]))
+
+instance Functor Parser where
+    fmap f (Parser p) = Parser $ first f <$> p
+
+instance Applicative Parser where
+    pure a = Parser (a,)
+    liftA2 :: (a -> b -> c) -> Parser a -> Parser b -> Parser c
+    liftA2 f (Parser pa) (Parser pb) = Parser $ pc . second pb . pa
+      where
+        pc (a, (b, bs)) = (f a b, bs)
+
+instance Monad Parser where
+    (>>=) :: Parser a -> (a -> Parser b) -> Parser b
+    Parser pa >>= f = Parser $ pb . pa
+      where
+        pb (a, bs) = let Parser parser = f a in parser bs
+
+-- Readable
+class Readable a where
+    read :: ByteString -> a
+
+instance Readable Integer where
+    read s = let Just (res, _) = BS.readInteger s in res
+
+next :: Readable a => Parser a
+next = Parser (\(b : bt) -> (read b, bt))
 
 main :: IO ()
-main = interact $ show . please . words
+main = BS.interact $ tostr . fst . p . BS.words
   where
-    please = solve . fmap read
+    Parser p = aa <$> next <*> next
+    tostr = BS.pack . show
 
-solve :: [I] -> I
-solve [a, b] = div (a + b - 1) b
+aa :: Integer -> Integer -> Integer
+aa a b = div (a + b - 1) b
