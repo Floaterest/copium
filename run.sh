ebb23d9714200cd1a87b9042e99b3031dd0edb5f
floaterest <floaterest@users.noreply.github.com>
2023-11-26 21:03:48 -0500

    feat(run) back to bash

---
+++ run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+run() {
+    bat input.txt
+	if [ -f "package.yaml" ]; then
+		stack run
+	elif [ -f "Cargo.toml" ]; then
+		cargo run
+	else
+		echo 'No project found'
+	fi < input.txt | bat --file-name output.txt
+}
+
+input=$1
+while [[ "$input" != 'q' ]]; do
+    case "$input" in
+        q)
+            exit 0 ;;
+        r)
+            run ;;
+    esac
+    printf '>>> '
+    read input
+done
